f4e91c6987fac9814a9a47e48328dec91cebc344
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-02-25 15:00:46 +0000

    finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,4 +3,33 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static boolean isPalindrome(String phrase){
+		String word = phrase.replaceAll(" ", "").toLowerCase();
+		for (int i = 0; i < word.length(); i++){
+			if (word.charAt(i) != word.charAt(word.length()-1-i)){
+				return false;
+			}
+		}
+return true;
+	}
+
+public static int getAge(String[] nameList, int[] ages, String name){
+for (int i = 0; i < nameList.length; i++){
+	if(nameList[i].equals(name)){
+		return ages[i];
+	}
+}
+return -1;
+}
+
+public static int countWords(String phrase, char c){
+	int counter  = 0;
+	String[] stringArr = phrase.split(" ");
+	for (int i = 0; i < stringArr.length; i++){
+		if(stringArr[i].contains("" + c)){
+			counter ++;
+		}
+	}
+	return counter;
+}
 }
